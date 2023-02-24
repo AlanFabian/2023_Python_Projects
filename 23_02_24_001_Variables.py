6c7898d034c16b8becea55f565986827d4d7fa7f
Alanfabian <63133225+AlanFabian@users.noreply.github.com>
2023-02-24 14:28:15 -0600

    Add files via upload

---
+++ 23_02_24_001_Variables.py
@@ -0,0 +1,12 @@
+#Alan de Jesus Fabian Garcia  24/02/2023
+#Ejemplo
+mensaje = "Bienvenidos al curso de Python" #Asignar la cadena de texto a la variable mensaje 
+mensaje = 10#Asignar un valor numerico a la variable mensaje 
+print(mensaje+20)#Imprimir en pantalla el valor de la variable mensaje +20 es decir 10+20=30
+#Seccion ejercicios Python 
+mensaje = "Hola espero estes muy bien"
+print(mensaje)
+Numero1 = 5
+Numero2 = 10
+ResultadoSuma=Numero1+Numero2#Realizar la suma de dos variables con valores numericos almacenandolo en una tercera variable 
+print("El resultado de la suma de dos numeros es:",ResultadoSuma)#Impresion en pantalla con mensaje al usuario + una variable 
---
+++ 23_02_24_002_Strings.py
@@ -0,0 +1,51 @@
+#Alan de Jesus Fabian Garcia  24/02/2023
+#Ejemplo
+mensaje = "esto es un'String'"#fn?
+print(mensaje)
+#Seccion Ejercicios de Python
+mensaje1="Este mensaje utiliza comillas dobles"
+print(mensaje1)
+mensaje2='Este mensaje utiliza comillas simples'
+print(mensaje2)
+"print()"
+#Capitulo 4 Concatenar strings 2
+palabra1 = 'Programacion'
+palabra2 = 'Facil'
+nombre_completo = palabra1+ ' ' +palabra2
+print(nombre_completo)
+#Si se concatenan numeros estos se sumaran 
+num1 = 10
+num2 = 15
+suma = num1+num2
+print(suma)
+#Si se busca juntar numeros de tal manera que queden asi 1510
+num1 = '10'
+num2 = '15'
+suma= num1+num2
+print(suma)
+#Seccion Ejercicios de Python
+#Concatena dos strings en una variable. Puedes comprobar el resultado con un print().
+palabra3 = 'Buenas '+ '' +' Noches '
+print(palabra3)
+#Concatena dos variables con strings en una tercera variable.
+palabra1 = 'Buenas '
+palabra2 = 'Tardes'
+mensaje_completo= palabra1+ '' +palabra2
+print(mensaje_completo)
+#Concatena una variable con string y un string directamente en un print().
+palabra3= "Hola"
+print(palabra3,"Como estas?")
+#Escribe tu nombre y apellidos separados en variables. Después concatena estas variables dentro de otra. No te olvides de concatenar los espacios entre el nombre y cada apellido. Finalmente, imprime el resultado.
+Nombre1 = "Alan"
+Nombre2 = " De Jesus "
+Apellido1= " Fabian "
+Apellido2= " Garcia "
+Nombre_completo= Nombre1+ '' +Nombre2+ '' +Apellido1+ '' +Apellido2
+print(Nombre_completo)
+#Concatena dos números, no los sumes.
+numero1 = '20'
+numero2 = '15'
+Numeros = numero1+numero2
+print(Numeros)
+
+
